7e8760928734870dcd7ca6cec492207cdac0fc7b
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2024-11-16 14:14:54 +0100

    storage of application status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField } from '@mui/material';
+import { FormControl, RadioGroup, FormControlLabel, Radio, TextField } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,8 +17,23 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 function App() {
+  const [cat, setCat] = useState('');
+  const [image, setImage] = useState('');
+  const [radio, setRadio] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorStatus, setErrorStatus] = useState(null);
+
+  const handleChange = (e) => {
+    const target = e.target;
+    if (target.checked) {
+      setRadio(target.value);
+    }
+  };
+
+  console.log(cat);
+
   return (
-    <Box className= 'child'>
+    <Box className="child">
       <Grid container spacing={2}>
         <Grid item xs={6}>
           <Item>
@@ -27,18 +43,22 @@ function App() {
                 type="text"
                 id="Cat"
                 variant="outlined"
+                value={cat}
+                onChange={(e) => setCat(e.target.value)}
               />
-              <RadioGroup name="radio-buttons-group">
-                <FormControlLabel type="text" value="red" control={<Radio />} label="Red" />
-                <FormControlLabel type="text" value="green" control={<Radio />} label="Green" />
-                <FormControlLabel type="text" value="blue" control={<Radio />} label="Blue" />
-      
-               </RadioGroup>
+              <RadioGroup
+                name="radio-buttons-group"
+                value={radio}
+                onChange={(e) => setRadio(e.target.value)}
+              >
+                <FormControlLabel value="red" control={<Radio />} label="Red" />
+                <FormControlLabel value="green" control={<Radio />} label="Green" />
+                <FormControlLabel value="blue" control={<Radio />} label="Blue" />
+              </RadioGroup>
             </FormControl>
           </Item>
         </Grid>
-        
-          <Grid item xs={6}>
+        <Grid item xs={6}>
           <Item>
             <h2>Cat could not be generated</h2>
           </Item>
